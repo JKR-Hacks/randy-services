6cab34583bda58ca7ca76d43d31e37cb71106061
Randy <rlthomasccv@gmail.com>
2018-12-20 15:34:32 -0800

    config eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+const sampleData = require('./Sample_Data.js');
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-
-
-
-    render() {
-        return //();
+  constructor(props) {
+    super(props);
+    this.state = {
+      sample: sampleData,
     };
-};
+  }
+
+  wtf() {
+    console.log('hello');
+  }
+
+
+  render() {
+    return (
+      this.wtf()
+    );
+  }
+}
 
 export default App;
