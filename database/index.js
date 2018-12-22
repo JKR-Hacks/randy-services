169fc3280d75ddd88705b034616f154ad810cd3f
Randy <rlthomasccv@gmail.com>
2018-12-22 10:26:21 -0800

    Database change - set up seed

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/tattooAssistant/newClient');
+mongoose.connect('mongodb://localhost/espn/stats');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
