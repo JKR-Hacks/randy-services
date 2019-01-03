c72124945808f5315e64d16501ffd405551f48dc
Randy <rlthomasccv@gmail.com>
2019-01-02 19:10:10 -0800

    Refactored Simplified

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import Offense from './components/Offense.jsx';
-import Navbar from './components/Navbar.jsx';
-import Defense from './components/Defense.jsx';
-import SpecialTeams from './components/SpecialTeams.jsx';
 import MainComponent from './components/MainComponent.jsx';
 
-const sampleData = require('./Sample_Data.js');
+// const sampleData = require('./Sample_Data.js');
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // sample: sampleData,
-      sample: '',
-      photos: '',
-      view: 'home',
+      sample: [],
     };
   }
 
-
   componentDidMount() {
-    $.ajax({
-      method: 'GET',
-      url: 'https://randomuser.me/api/?gender=male&results=6',
-      dataType: 'json',
-      error: (err) => {
-        console.log(err, 'error');
-      },
-      success: (data) => {
-        this.setState({
-          photos: data,
-        });
-      },
-    });
     $.ajax({
       method: 'GET',
       url: '/stats',
@@ -46,65 +25,13 @@ class App extends Component {
         });
       },
     });
-    this.renderView();
   }
-
-
-  changeView(e) {
-    this.setState({
-      view: e,
-    });
-    this.renderView();
-  }
-
-
-  renderView() {
-    const { sample, view, photos } = this.state;
-    if (view === 'home') {
-      return (
-        <div id="homePage">
-          <a href="https://www.therams.com/" rel="opener" target="_blank">
-            <img src="https://pngimage.net/wp-content/uploads/2018/06/la-rams-logo-png-5.png" width="400" height="190" alt="" />
-          </a>
-          <br />
-          <br />
-          <h1>Welcome To The Official L.A. Rams Statistics App!</h1>
-          <h4>Brought to you by ESPN</h4>
-        </div>
-      );
-    } if (view === 'offense') {
-      return (
-        <div>
-          <Offense sample={sample} />
-        </div>
-      );
-    } if (view === 'defense') {
-      return (
-        <div>
-          <Defense sample={sample} />
-        </div>
-      );
-    } if (view === 'specialTeams') {
-      return (
-        <div>
-          <SpecialTeams sample={sample} />
-        </div>
-      );
-    } if (view === 'mainComponent') {
-      return (
-        <div>
-          <MainComponent sample={sample} photos={photos.results} />
-        </div>
-      );
-    }
-  }
-
 
   render() {
+    const { sample } = this.state;
     return (
       <div>
-        <Navbar change={this.changeView.bind(this)} />
-        {this.renderView()}
+        <MainComponent sample={sample} />
       </div>
     );
   }
