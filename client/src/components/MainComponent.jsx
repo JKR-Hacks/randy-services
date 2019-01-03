48b98a06f993822f565438898317cbf2e12aba98
Randy <rlthomasccv@gmail.com>
2019-01-02 20:31:16 -0800

    Finalized Stats Component

--- client/src/components/MainComponent.jsx
+++ client/src/components/MainComponent.jsx
@@ -2,38 +2,41 @@ import React, { Component } from 'react';
 
 const MainComponent = ({ sample }) => (
 
-  <div id="container" className="container">
+  <div id="container" className="col-lg-3 alert alert-light container">
+    <p className="text-dark font-weight-bold text-left">Team Stats</p>
     <div className="row">
 
-      <div id="lefttop" className="col-md-2">
+      <div id="lefttop" className="col-sm border-right border-bottom border-secondary text-center">
         <p id="headingComp">Passing:</p>
         <br />
-        {sample.reduce((elem, i) => elem + i.Offense.Passing, 0) / sample.length}
+        <p className="h4 text-dark">{sample.reduce((elem, i) => elem + i.Offense.Passing, 0) / sample.length}</p>
       </div>
 
-      <div id="righttop" className="col-md-2">
+      <div id="righttop" className="col-sm border-bottom border-secondary text-center">
         <p id="headingComp">Rushing:</p>
         <br />
-        {sample.reduce((elem, i) => elem + i.Offense.Rushing, 0) / sample.length}
+        <p className="h4 text-dark">{sample.reduce((elem, i) => elem + i.Offense.Rushing, 0) / sample.length}</p>
       </div>
 
     </div>
 
     <div className="row">
 
-      <div id="leftbottom" className="col-md-2">
+      <div id="leftbottom" className="col-sm border-right border-bottom border-secondary text-center">
         <p id="headingComp">Tackles:</p>
         <br />
-        {sample.reduce((elem, i) => elem + i.Defense.Tackles, 0) / sample.length}
+        <p className="h4 text-dark">{sample.reduce((elem, i) => elem + i.Defense.Tackles, 0) / sample.length}</p>
       </div>
 
-      <div id="rightbottom" className="col-md-2">
+      <div id="rightbottom" className="col-sm border-bottom border-secondary text-center">
         <p id="headingComp">Interceptions:</p>
         <br />
-        {sample.reduce((elem, i) => elem + i.Defense.Interceptions, 0) / sample.length }
+        <p className="h4 text-dark">{sample.reduce((elem, i) => elem + i.Defense.Interceptions, 0) / sample.length}</p>
       </div>
 
     </div>
+    <br />
+    <a href="http://www.espn.com/nfl/statistics" target="_blank"><p className="text-center">All Team Rankings</p></a>
 
   </div>
 
